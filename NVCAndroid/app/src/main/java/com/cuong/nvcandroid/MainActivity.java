ff832556e78822e6b458fb3d094989b1cda155f3
gacon.nhonho <unme.rf@gmail.com>
2016-07-02 10:56:43 +0700

    pp

--- NVCAndroid/app/src/main/java/com/cuong/nvcandroid/MainActivity.java
+++ NVCAndroid/app/src/main/java/com/cuong/nvcandroid/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         ToastUtils.show(this, "Good");
         ToastUtils.show(this, "Good");
         ToastUtils.show(this, "Very Good");
+        ToastUtils.show(this, "Very Good");
         PermissionUtils.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.READ_CONTACTS,
